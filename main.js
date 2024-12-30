b87491e88229d68cc8179b65eff1a46528aeb008
Anas Farooq <anasfarooq2006@gmail.com>
2024-12-31 01:43:05 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,26 +3,24 @@
 const preloader = document.getElementById("preloader");
 
 // Function to hide the loader
+
 function hideLoader() {
-    preloader.style.display = 'none'; // Hide preloader
-    document.body.style.overflow = 'auto'; // Restore scrolling
+    preloader.style.display = 'none'; 
+    document.body.style.overflow = 'auto'; 
 }
 
 // Function to show the loader
 function showLoader() {
-    preloader.style.display = 'grid'; // Show preloader
-    document.body.style.overflow = 'hidden'; // Block scrolling
+    preloader.style.display = 'grid'; 
+    document.body.style.overflow = 'hidden'; 
 }
 
-// Show the loader initially
 showLoader();
 
-// Set a timeout to hide the loader after 1 second (adjust as necessary)
 const hideLoaderTimeout = setTimeout(hideLoader, 1000);
 
-// Event listener to hide the loader when the window is fully loaded
 window.addEventListener('load', function() {
-    // If loading is completed before 1 second, clear the timeout and ensure loader stays visible for the remaining time
+
     if (hideLoaderTimeout) {
         clearTimeout(hideLoaderTimeout);
         const timeElapsed = Date.now() - window.performance.timing.navigationStart;
